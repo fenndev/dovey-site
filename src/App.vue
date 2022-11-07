dbd158db1de6d6ab573228b9da7cfa806bd46a6a
fenndev <fenndev@protonmail.com>
2022-11-07 07:18:35 -0800

    Progress on Link components

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,12 @@ import Link from './components/Link.vue'
 </script>
 
 <template>
-  <Link to="#" />
+  <Link to="#" icon="/src/assets/vue.svg" icon-alt-text="The Vue logo" link-text="Vue"/>
 </template>
+
+<style>
+  div {
+    display: flex;
+    justify-content: center;
+  }
+</style>
--- src/components/Link.vue
+++ src/components/Link.vue
@@ -1,25 +1,69 @@
 <script lang="ts">
 export default {
-    // props: {
-    //     to: {
-    //         type: String,
-    //         required: true
-    //     },
-    //     target: {
-    //         type: String,
-    //         default: '_self'
-    //     },
-    //     rel: {
-    //         type: String,
-    //         default: 'noopener noreferrer'
-    //     }
-    // }
+    props: {
+        to: {
+            type: String,
+            required: true
+        },
+        icon: {
+            type: String,
+            required: true,
+            default: ''
+            
+        },
+        iconAltText: {
+            type: String,
+            required: true,
+            default: 'An img!!!'
+        },
+        linkText: {
+            type: String,
+            required: true,
+            default: 'A link!!!'
+        }
+    }
 }
 </script>
 
 <template>
-    <h1>Hello world</h1>
-  <!-- <a :href="to" :target="target" :rel="rel">
-    <slot />
-  </a> -->
+  <a :href="to" >
+    <img :src="icon" :alt="iconAltText">
+    <p>{{linkText}}</p>
+  </a>
 </template>
+
+<style scoped>
+a {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 250px;
+    height: 50px;
+    border: 1px solid black;
+    border-radius: 10px;
+    margin: 10px;
+    padding: 20px;
+}
+
+a, a:visited {
+  color: black;
+  text-decoration: none;
+}
+
+a:hover {
+    background-color: #42b983;
+    color: #e8f5e9;
+}
+
+img {
+    width: 35px;
+    height: 35px;
+}
+
+p {
+    text-align: center;
+    margin: 0;
+    font-size: 1.5rem;
+}
+</style>
